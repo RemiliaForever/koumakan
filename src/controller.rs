2d7fb7472b9cb17f769780af998e9f0a704cdc24
RemiliaForever <remilia@koumakan.cc>
2017-08-24 15:36:19 +0800

    remove template and static files support

--- src/controller.rs
+++ src/controller.rs
@@ -3,7 +3,6 @@ extern crate rocket_contrib;
 extern crate serde_json;
 
 use diesel::prelude::*;
-use rocket_contrib::Template;
 use rocket_contrib::Json;
 
 use db::DbConn;
@@ -13,11 +12,6 @@ use models::comment::dsl::*;
 #[get("/")]
 fn root(conn: DbConn) -> Json<Vec<Comment>> {
     Json(comment.load::<Comment>(&*conn).expect("query error!"))
-}
-
-#[get("/index")]
-fn index() -> Template {
-    Template::render("index", ("a", "a"))
 }
 
 #[get("/<all>")]
--- src/main.rs
+++ src/main.rs
@@ -14,19 +14,12 @@ extern crate chrono;
 extern crate rocket;
 extern crate rocket_contrib;
 
-use rocket_contrib::Template;
-use std::path::{Path, PathBuf};
-use rocket::response::NamedFile;
 
 mod db;
 mod models;
 mod controller;
 use controller::*;
 
-#[get("/<file..>")]
-fn files(file: PathBuf) -> Option<NamedFile> {
-    NamedFile::open(Path::new("static/").join(file)).ok()
-}
 
 #[error(404)]
 fn not_found() -> String {
@@ -42,10 +35,8 @@ fn main() {
     let server = rocket::ignite();
     let dbpool = db::init();
     server
-        .mount("/", routes![root, index, all])
-        .mount("/static", routes![files])
+        .mount("/", routes![root, all])
         .catch(errors![not_found, server_error])
-        .attach(Template::fairing())
         .manage(dbpool)
         .launch();
 
